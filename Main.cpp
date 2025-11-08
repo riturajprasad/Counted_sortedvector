95a38bcf5a8fc35fb0b048ff110bb3572459b321
Rituraj Prasad <rituraj.prasad2718@gmail.com>
2025-11-08 17:07:12 +0530

    make counted_sortedvector

---
+++ Main.cpp
@@ -0,0 +1,32 @@
+#include "counted_sortedvector.h"
+
+int main()
+{
+	counted_sortedvector<int> csv1;
+	csv1.insert(5);
+	csv1.insert(1);
+	csv1.insert(3);
+	csv1.insert(9);
+	csv1.insert(3);
+
+	cout << "Index of " << 3 << " is " << csv1.findIndex(3) << endl;
+	if (csv1.find(7))
+		cout << 7 << " Found" << endl;
+	else
+		cout << 7 << " Not found" << endl;
+	cout << "Count of " << 3 << " is " << csv1.count(3) << endl;
+	cout << "Print all value: " << endl;
+	csv1.printall();
+	cout << "Total size = " << csv1.total_size() << endl;
+	cout << "Unique size = " << csv1.unique_size() << endl;
+
+	csv1.erase(1);
+	cout << "Print all value (after erase(1)): " << endl;
+	csv1.printall();
+
+	csv1.clear();
+	cout << "Print all value (after clear()): " << endl;
+	csv1.printall();
+
+	return 0;
+}
--- counted_sortedvector.h
+++ counted_sortedvector.h
@@ -21,12 +21,111 @@ public:
         return *this;
     }
 public:
+    bool isempty() const;
+    int findIndex(const T& value) const;
     void insert(T value);
     bool find(T value) const;
     int count(T value) const;
-    void printall() const;
-    int size(T value) const;
-    int findIndex(const T& value) const;
     void erase(T value);
-    void clear(T value);
+    void clear();
+    void printall() const;
+    int total_size() const;
+    int unique_size() const;
 };
+
+template <class T>
+bool counted_sortedvector<T>::isempty() const
+{
+    if (data.size() == 0)
+    {
+        cout << "counted_sortedvector is empty" << endl;
+        return true;
+    }
+    return false;
+}
+
+template <class T>
+int counted_sortedvector<T>::findIndex(const T& value) const {
+    int low = 0, high = data.size() - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (data[mid].first == value) return mid;
+        else if (data[mid].first < value) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+template <class T>
+void counted_sortedvector<T>::insert(T value) {
+    int index = findIndex(value);
+    if (index != -1) {
+        data[index].second++;
+    }
+    else {
+        auto it = lower_bound(
+            data.begin(), data.end(),
+            make_pair(value, 1),
+            [](const pair<T, int>& a, const pair<T, int>& b) {
+                return a.first < b.first;
+            }
+        );
+        data.insert(it, { value, 1 });
+    }
+}
+
+template <class T>
+bool counted_sortedvector<T>::find(T value) const {
+    if (isempty()) return false;
+    return findIndex(value) != -1;
+}
+
+template <class T>
+int counted_sortedvector<T>::count(T value) const {
+    if (isempty()) return 0;
+    int index = findIndex(value);
+    if (index != -1) return data[index].second;
+    return 0;
+}
+
+template <class T>
+void counted_sortedvector<T>::erase(T value) {
+    if (isempty()) return;
+    int index = findIndex(value);
+    if (index != -1) {
+        data[index].second--;
+        if (data[index].second == 0) {
+            data.erase(data.begin() + index);
+        }
+    }
+}
+
+template <class T>
+void counted_sortedvector<T>::clear() {
+    if (isempty()) return;
+    data.clear();
+}
+
+template <class T>
+void counted_sortedvector<T>::printall() const {
+    if (isempty()) return;
+    for (auto& p : data) {
+        for (int i = 0; i < p.second; i++)
+            cout << p.first << " ";
+    }
+    cout << endl;
+}
+
+template <class T>
+int counted_sortedvector<T>::total_size() const {
+    if (isempty()) return 0;
+    int sum = 0;
+    for (auto& p : data) sum += p.second;
+    return sum;
+}
+
+template <class T>
+int counted_sortedvector<T>::unique_size() const {
+    if (isempty()) return 0;
+    return data.size();
+}
